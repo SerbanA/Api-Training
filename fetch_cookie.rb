c3becefb3690424e38e10569237bf3fcd099533e
SerbanA <adyserban97@yahoo.com>
2019-07-17 11:51:49 +0300

    Added explanations for each segment of the code

--- fetch_cookie.rb
+++ fetch_cookie.rb
@@ -5,11 +5,12 @@ uri = URI.parse("https://www.ifep.ro/Account/Login.aspx") #url to be parsed
 http = Net::HTTP.new(uri.host, uri.port) #new http class
 puts uri.host # show site host => ifep.ro
 puts uri.port # show used port => 443
+puts uri.path # shows the path => /Account/Login.aspx
 http.use_ssl = true #Turn on/off SSL. This flag must be set before starting session
 headers = { # headers used 
     'cookie' => '_gat=1' # cookie used to stop the redirection to another page and request a new sessionID cokie
 } 
-puts uri.path # shows the path => /Account/Login.aspx
+
 resp = http.request_get(uri.path, headers) # making a new get request using the path and the cookie inside the header
 puts resp.to_hash # shows the response from the get request
 =begin
