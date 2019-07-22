954c399b4243d97b63a27d238e3217ccf69fc716
SerbanA <adyserban97@yahoo.com>
2019-07-22 12:34:36 +0300

    Combined into 1 module(Ifep)

--- ifep/Cookie.rb
+++ ifep/Cookie.rb
@@ -1,8 +1,8 @@
 require 'uri'
 require 'net/http'
 
-module Cookie
-    def self.get_cookie(headers)
+module Ifep
+    def self.obtaining_cookie(headers)
         uri = URI.parse("https://www.ifep.ro/Account/Login.aspx")
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
---
+++ ifep/Filters.rb
@@ -0,0 +1,17 @@
+params = {
+    "prefixText": "",
+    "count": 0,
+    "contextKey": "null"
+}
+headers = { 
+    'Content-Type' =>'application/json',
+    'Accept'=>'application/json',
+    'cookie' => '_gat=1'
+}
+=begin
+headers["cookie"] = Cookie.get_cookie(headers)
+
+params["prefixText"] = gets.chomp
+
+params["count"] = gets.to_i
+=end
