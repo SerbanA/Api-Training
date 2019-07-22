a0a0fa35f2467bad5b5e4407faa67cdac4eace0a
SerbanA <adyserban97@yahoo.com>
2019-07-22 14:09:38 +0300

    File used to store the filters

--- ifep/Filters.rb
+++ ifep/Filters.rb
@@ -8,10 +8,9 @@ headers = {
     'Accept'=>'application/json',
     'cookie' => '_gat=1'
 }
+
 =begin
 headers["cookie"] = Cookie.get_cookie(headers)
-
 params["prefixText"] = gets.chomp
-
 params["count"] = gets.to_i
 =end
