ec47bbb818b06729d57302b7a6a72b2b13a82843
SerbanA <adyserban97@yahoo.com>
2019-08-02 16:33:09 +0300

    3.0 need to finis for barou and adress

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,32 @@
 require_relative 'ifep'
+require 'nokogiri'
+
 headers = Ifep::Variables.headers
 params = Ifep::Variables.params
-temp = Ifep::Variables.form
-body = temp["body"]
+body = Ifep::Variables.body
+
 def updating_cookie(cookie,headers)
     headers['cookie'] = cookie
     headers
+end
+
+def parsing(input)
+    document = Nokogiri::HTML(input)
+    puts
+    job = document.search('h4 span[class^="label label"]')[0].text
+    nume = document.search('.col-md-12 h4 [style="font-weight:bold;"]')[0].text
+   # barou = document.search('.col-md-12 [class="text-nowrap"]')[0].text
+    stare = document.search('h4 span[style^="color:"]')[0].text
+    #adress = document.search('.col-md-12 [class="fas fa-map-marker text-red padding-right-sm"]')[0].text
+    phone = document.search('.col-md-12 [class="padding-right-md text-primary"]')[0].text
+    mail = document.search('.col-md-12 [class="text-nowrap"]')[0].text
+    puts job
+    puts nume
+    #puts barou
+    puts stare
+   # puts adress
+    puts phone
+    puts mail
 end
 
 command = Ifep::ObtainCookie.call(headers)
@@ -17,7 +38,7 @@ if command.success?
     command = Ifep::Lawyers.call(headers, body)
     if command.success?
      lawyers = command.result 
-     puts lawyers
+     parsing(lawyers)
      else
      puts command.errors[:fetch_lawyers]
     end
